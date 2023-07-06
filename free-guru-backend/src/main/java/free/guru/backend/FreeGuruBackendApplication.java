c8de7ae14e09106f34d80f2a8f4c841864d5b0c2
Aprijal Ghiyas Setiawan <aprijalghiyas@gmail.com>
2023-07-06 14:19:36 +0700

    First commit

---
+++ free-guru-backend/src/main/java/free/guru/backend/FreeGuruBackendApplication.java
@@ -0,0 +1,13 @@
+package free.guru.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeGuruBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeGuruBackendApplication.class, args);
+	}
+
+}
---
+++ free-guru-backend/src/test/java/free/guru/backend/FreeGuruBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package free.guru.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeGuruBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
